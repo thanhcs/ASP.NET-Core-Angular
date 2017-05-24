726d9fa11f1c7613bd7778ea1fbcc675ae921b0e
thanhcs <thanhnguyencs@utexas.edu>
2017-05-24 16:29:57 -0500

    Add project files.

---
+++ ClientApp/app/app.module.shared.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './components/app/app.component'
+import { NavMenuComponent } from './components/navmenu/navmenu.component';
+import { HomeComponent } from './components/home/home.component';
+import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
+import { CounterComponent } from './components/counter/counter.component';
+import { TopNavMenuComponent } from './components/topnavmenu/topnavmenu.component' ;
+
+export const sharedConfig: NgModule = {
+    bootstrap: [ AppComponent ],
+    declarations: [
+        AppComponent,
+        NavMenuComponent,
+        CounterComponent,
+        FetchDataComponent,
+        HomeComponent,
+        TopNavMenuComponent
+    ],
+    imports: [
+        RouterModule.forRoot([
+            { path: '', redirectTo: 'home', pathMatch: 'full' },
+            { path: 'home', component: HomeComponent },
+            { path: 'counter', component: CounterComponent },
+            { path: 'fetch-data', component: FetchDataComponent },
+            { path: '**', redirectTo: 'home' }
+        ])
+    ]
+};
---
+++ ClientApp/app/components/topnavmenu/topnavmenu.component.ts
@@ -0,0 +1,9 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'top-nav-menu',
+    templateUrl: './topnavmenu.component.html',
+    styleUrls: ['./topnavmenu.component.css']
+})
+export class TopNavMenuComponent {
+}
